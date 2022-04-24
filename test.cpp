f03f4c6649885fa1ebbea4243a13e83bebfef482
TheEvilDemonicFlower <104311675+TheEvilDemonicFlower@users.noreply.github.com>
2022-04-24 17:59:22 -0400

    Added test.cpp
    test.cpp is used to test the Buffer and Player class, has some example code for interactions between functions and user.

---
+++ test.cpp
@@ -0,0 +1,45 @@
+//Used to test both the Buffer and Player class, has some example code for how to interact with functions!
+
+
+#include <iostream>
+
+#include "Buffer.h"
+#include "Player.h"
+
+
+using namespace std;
+int main() {
+	Buffer buffer;
+	buffer.current_year_set(2000);
+	buffer.load_data("save_data.txt");
+	//buffer.test_cout();
+	buffer.add_entry("bob", "jefferson", 2010, false);
+	//buffer.print_data("save_data.txt");
+
+	vector<Player*> results;
+
+	cout << "Search A:" << endl;
+	buffer.search(results, "", "", "", 0, 12, false);
+	for (auto x : results)
+	{
+		cout << *x;
+	}
+	cout << "Search B:" << endl;
+	buffer.search(results, "", "", "a", 0, 0);
+	for (auto x : results)
+	{
+		cout << *x;
+	}
+	int stats[8]{};
+	buffer.statistics(stats);
+	cout << "total: " << stats[0] << endl;
+	cout << "paid for: " << stats[1] << endl;
+	cout << "paid for u6: " << stats[2] << endl;
+	cout << "paid for u8: " << stats[3] << endl;
+	cout << "paid for u10: " << stats[4] << endl;
+	cout << "paid for u12: " << stats[5] << endl;
+	cout << "paid for u14: " << stats[6] << endl;
+	cout << "paid for u17: " << stats[7] << endl;
+
+	return 0;
+ }
