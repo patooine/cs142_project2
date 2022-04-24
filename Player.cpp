8ed3da172e00f661f053ea5b803c1307e9735ebe
TiresomeCube34 <71302309+TiresomeCube34@users.noreply.github.com>
2022-04-24 13:15:07 -0400

    Refined final player

--- Player.cpp
+++ Player.cpp
@@ -1,17 +1,51 @@
 #include "Player.h"
-
 using namespace std;
 
-void set_catagory(const int& new_catagory) {
-	switch (new_catagory) {
+void Player::set_category(const int& new_category) {
+	switch (new_category) {
 	case 6:
-		catagory_ = u6;
+		u = u6;
+	case 8:
+		u = u8;
+	case 10:
+		u = u10;
+	case 12:
+		u = u12;
+	case 14:
+		u = u14;
+	case 17:
+		u = u17;
 	}
 }
 
-istream& operator >> (istream& in, Player &p) {
-	in >> p.nameFirst_;
-	in >> p.nameLast_;
+
+int Player::category() {
+	switch (u) {
+	case u6: 
+		return '6';
+	case u8:
+		return '8';
+	case u10:
+		return '10';
+	case u12:
+		return '12';
+	case u14:
+		return '14';
+	case u17:
+		return '17';
+	}
+}
+
+istream& operator>>(istream& in, Player& p) {
+	getline(in, p.nameFirst_);
+	getline(in, p.nameLast_);
+	in >> p.year_;
+	in.ignore();
+	int stopGap;
+	in >> stopGap;
+	p.set_category(stopGap);
+	in >> p.register_;
 	return in;
 }
+
 
--- Player.h
+++ Player.h
@@ -1,31 +1,34 @@
 
 #ifndef _Player_h_
-#define _player_h_
+#define _Player_h_
 
+#include<iostream>
 #include<string>
 class Player {
-	friend istream& operator >> (istream& in, Player p);
+	Player(std::string firstName, std::string lastName, int year, int catagory, bool registry) : nameFirst_(firstName), nameLast_(lastName), year_(year), register_(registry) { set_category(catagory); }
+	
+	friend std::istream& operator >> (std::istream& in, Player& p);
+	//friend enum category_ parse_category(const int& new_catagory);
 public:
 	inline int year() { return year_; }
-	inline string nameFirst() { return nameFirst_;}
-	inline string nameLast() { return nameLast_; }
-	inline string catagory()  { return catagory_; }
-	inline bool registery() { return register; }
+	inline std::string nameFirst() { return nameFirst_; }
+	inline std::string nameLast() { return nameLast_; }
+	int category();
+	inline bool registery() { return register_; }
 	inline void set_year(const int& new_year) { if (new_year != NULL) { year_ = new_year; } }
 	inline void set_nameFirst(const std::string& new_name) { if (new_name != "") { nameFirst_ = new_name; } }
 	inline void set_nameLast(const std::string& new_name) { if (new_name != "") { nameLast_ = new_name; } }
-	inline void set_register(const bool& new_registery) {  register_ = new_registery;  }
-	void set_catagory(const int& new_catagory);
+	inline void set_register(const bool& new_registery) { register_ = new_registery;  }
+	void set_category(const int& new_catagory);
 
 private:
-	int year_;
-	string nameFirst_, nameLast_;
-	enum catagory_
+	enum category_
 	{
-		u6, u8, u10, u12, u14, u17 
+		u6, u8, u10, u12, u14, u17
 	};
+	int year_;
+	std::string nameFirst_, nameLast_;
+	category_ u;
 	bool register_;
-
-
-}
-#endif 
+};
+#endif
